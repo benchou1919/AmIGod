df00a9f53b52468739d4677c72e1ba032393e62c
sycLin <b01902044@ntu.edu.tw>
2016-06-13 19:23:34 +0800

    + add 'posts' demo section in search page

--- AmIGod.py
+++ AmIGod.py
@@ -12,8 +12,10 @@ def index():
 def search():
 	global tClient
 	blog_url = request.form['blog_url']
-	detail = tClient.getBlogInfo(blog_url)['blog']
-	return render_template('search.html', blog_name=blog_url, detail=detail), 200
+	info = tClient.getBlogInfo(blog_url)['blog']
+	# posts = tClient.getBlogPostById(blog_name=blog_url, post_id=145759083652, include_reblog=True, include_notes=True)
+	posts = tClient.getBlogPostsByTag(blog_name=blog_url, tag="magneto", limit=100, offset=0, include_reblog=False, include_notes=False)
+	return render_template('search.html', blog_name=blog_url, info=info, posts=posts), 200
 
 if __name__ == "__main__":
 	app.run(port=8080)
