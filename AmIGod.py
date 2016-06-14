c85c6ecdf0d4430703ed96601bfe6a1cf5928e9a
Brian Huang <b01902068@ntu.edu.tw>
2016-06-14 18:22:37 +0800

    fix

--- AmIGod.py
+++ AmIGod.py
@@ -14,7 +14,7 @@ def search():
 	blog_url = request.form['blog_url']
 	info = tClient.getBlogInfo(blog_url)['blog']
 	# posts = tClient.getBlogPostById(blog_name=blog_url, post_id=145759083652, include_reblog=True, include_notes=True)
-	posts = tClient.getBlogPostsByTag(blog_name=blog_url, tag="magneto", limit=100, offset=0, include_reblog=False, include_notes=False)
+	posts = tClient.getBlogPostsByTag(blog_name=blog_url, tag="", limit=100, offset=0, include_reblog=False, include_notes=False)
 	return render_template('search.html', blog_name=blog_url, info=info, posts=posts), 200
 
 if __name__ == "__main__":
--- sample.py
+++ sample.py
@@ -8,25 +8,25 @@ TUMBLR_CONSUMER_KEY = "ytrXGiUjY2pZ9zggTPomZbXAwQD5xfveSj73h3bI76LwUkhYRk"
 TUMBLR_CONSUMER_SECRET = "PpHRSKUKqBMNp4bjchW8EkxQNcQvyVyDYDWiz7gROzVERsOYAs"
 
 # OAuth
-ACCESS_TOKEN = 'sSG3qISDXvahdO3qr8LbWy5SXXg3Ts0BBNRHCHDaksN9MSlbsy',
-ACCESS_SECRET = 'VCOF3mVfSQ7dHufNw3nCi5kW10bu5BrhGfgu8hlVU0Prvekr0R'
-# ACCESS_TOKEN = "v5e5U3tEOgZL37TGNbuhlUF3nMzaQV2zCVumRfd7wjkyN7ht4x"
-# ACCESS_SECRET = "FvHzFRwhZQLumDnrjXhvsiEDZ0mPSbbDtoFG15kX1x70MaFw8V"
+# ACCESS_TOKEN = 'sSG3qISDXvahdO3qr8LbWy5SXXg3Ts0BBNRHCHDaksN9MSlbsy',
+# ACCESS_SECRET = 'VCOF3mVfSQ7dHufNw3nCi5kW10bu5BrhGfgu8hlVU0Prvekr0R'
+ACCESS_TOKEN = "v5e5U3tEOgZL37TGNbuhlUF3nMzaQV2zCVumRfd7wjkyN7ht4x"
+ACCESS_SECRET = "FvHzFRwhZQLumDnrjXhvsiEDZ0mPSbbDtoFG15kX1x70MaFw8V"
 
 if __name__ == "__main__":
 	# create client
-	# client = pytumblr.TumblrRestClient(
-	# 	TUMBLR_CONSUMER_KEY,
-	# 	TUMBLR_CONSUMER_SECRET,
-	# 	ACCESS_TOKEN,
-	# 	ACCESS_SECRET
-	# )
 	client = pytumblr.TumblrRestClient(
-	  'ytrXGiUjY2pZ9zggTPomZbXAwQD5xfveSj73h3bI76LwUkhYRk',
-	  'PpHRSKUKqBMNp4bjchW8EkxQNcQvyVyDYDWiz7gROzVERsOYAs',
-	  'sSG3qISDXvahdO3qr8LbWy5SXXg3Ts0BBNRHCHDaksN9MSlbsy',
-	  'VCOF3mVfSQ7dHufNw3nCi5kW10bu5BrhGfgu8hlVU0Prvekr0R'
+		TUMBLR_CONSUMER_KEY,
+		TUMBLR_CONSUMER_SECRET,
+		ACCESS_TOKEN,
+		ACCESS_SECRET
 	)
+	# client = pytumblr.TumblrRestClient(
+	#   	'ytrXGiUjY2pZ9zggTPomZbXAwQD5xfveSj73h3bI76LwUkhYRk',
+	#   	'PpHRSKUKqBMNp4bjchW8EkxQNcQvyVyDYDWiz7gROzVERsOYAs',
+	#   	'sSG3qISDXvahdO3qr8LbWy5SXXg3Ts0BBNRHCHDaksN9MSlbsy',
+	#   	'VCOF3mVfSQ7dHufNw3nCi5kW10bu5BrhGfgu8hlVU0Prvekr0R'
+	# )
 	# get the client information
 	client_info = client.info()
 	client_dashboard = client.dashboard()
@@ -40,6 +40,7 @@ if __name__ == "__main__":
 	client_queue = client.queue('brianhuang1019.tumblr.com')
 	client_drafts = client.drafts('brianhuang1019.tumblr.com')
 	client_submission = client.submission('brianhuang1019.tumblr.com')
+	client_reblog = client.reblog('brianhuang1019.tumblr.com')
 	_client_posts = _client.posts('brianhuang1019.tumblr.com')
 
-	print json.dumps(tag_Data, indent=4)
+	print json.dumps(client_dashboard, indent=4)
