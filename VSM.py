2f234bde9d20201027a7fc5b6df75bd06e1dd13a
b01902002 <b01902002@ntu.edu.tw>
2016-06-27 05:57:38 +0800

    fix VSM.py

--- VSM.py
+++ VSM.py
@@ -1,100 +1,100 @@
-from tumblrUtil import TumblrAgent as TA
 from vocabUtil import VocabAgent as VA
 from collections import defaultdict
-import math
-
-BlogFre = defaultdict(float)
-db = 0.75
-dk = 2.5
+from math import sqrt, log
 
 class BlogVSM(object):
-	
+	# constructor
 	def __init__(self, blog_name):
 		self.blog_name = blog_name
-		self.blog_length = 0
-		self.relativity = 0
+		self.blog_length = float(0)
+		self.relativity = float(0)
+		self.TF = defaultdict(float)
 		self.vector = defaultdict(float)
+		self.vector_d = float(0);
 
-	def getBlogName(self):
-		return self.blog_name
-
-	def getBlogVector():
-		pass
-
-	def queryByBlogName():
-		pass
-
-if __name__ == "__main__":
-	global BlogFre, db, dk
-	totallength = 0
-	blog_num = 0
-
-	ta = TA()
-	Blogs = []
-
+class VSM(object):
+	# constructor
+	def __init__(self, ta=None, pb=0.75, pk=2.5, topK=10):
+		self.ta = ta
+		self.pb = pb
+		self.pk = pk
+		self.topK = topK
+		self.Blogs = []
+		self.BlogFre = defaultdict(float)
+		self.totallength = float(0)
+		self.avglength = float(0)
+		# initial run through
+		self.__goThroughAllPost()
 	# go through all blog's post
-	for bn in ta.getAllBlogs():
+	def __goThroughAllPost(self):
+		bn = self.ta.getAllBlogs()
+		for index in range(len(bn)):
+			self.Blogs.append(BlogVSM(bn[index]))
+			b = self.ta.getBlogByName(bn[index])
+			pid_list = b.getAllPosts()
+			termlist = []
+			for pid in pid_list:
+				p = self.ta.getPostById(bn[index], pid)
+				terms = VA.extractTermsFromPost(p)
+				termlist += terms
+				for term in terms:
+					self.Blogs[index].TF[term] += 1
+					self.Blogs[index].blog_length += 1
+					self.totallength += 1
+			for term in list(set(termlist)):
+				self.BlogFre[term] += 1
+		self.__countVectorWeight()
+	# count blog's vector weight
+	def __countVectorWeight(self):
+		self.avglength = self.totallength/len(self.Blogs)
+		# trans RawTF into NormTF*IDF
+		for bs in self.Blogs:
+			tempd = float(0)
+			for key,value in bs.TF.iteritems():
+				bs.vector[key] = (self.pk+1)*value / (value+self.pk*(1-self.pb+self.pb*bs.blog_length/self.avglength)) * log(len(self.Blogs)/self.BlogFre[key])
+				tempd += bs.vector[key] * bs.vector[key]
+			bs.vector_d = sqrt(tempd)
+	# query by blog name and return relative blogs
+	def queryByBlogName(self, queryBlog):
+		# blog already in Blogs
+		if queryBlog in self.ta.getAllBlogs():
+			for index in range(len(self.Blogs)):
+				if self.Blogs[index].blog_name == queryBlog:
+					Query = self.Blogs[index]
+					break
+		# blog not in Blogs
+		else:
+			Query = BlogVSM(queryBlog)
+			b = self.ta.getBlogByName(queryBlog)
+			pid_list = b.getAllPosts()
+			termlist = []
+			for pid in pid_list:
+				p = self.ta.getPostById(queryBlog, pid)
+				terms = VA.extractTermsFromPost(p)
+				termlist += terms
+				for term in terms:
+					Query.TF[term] += 1
+					Query.blog_length += 1
+					self.totallength += 1
+			# append to Blogs
+			self.Blogs.append(Query)
+			for term in list(set(termlist)):
+				self.BlogFre[term] += 1
+			# recount vector weight 
+			self.__countVectorWeight()
+			Query = self.Blogs[len(self.Blogs)-1]
 
-		Blogs.append(BlogVSM(bn))
-
-		b = ta.getBlogByName(bn)
-		pid_list = b.getAllPosts()
-		
-		for pid in pid_list:
-			
-			p = ta.getPostById(bn, pid)
-
-			terms = VA.extractTermsFromPost(p)
-
-			for term in terms:
-				Blogs[index].vector[term] += 1
-				Blogs[index].blog_length += 1
-				BlogFre[term] += 1
-				totallength += 1
-
-		blog_num += 1
-
-	# trans blog's vector weight RawTF into NormTF*IDF
-	avglength = totallength/blog_num
-	
-	for bs in Blogs:
-		for key,value in bs.vector.iteritems():
-			bs.vector[key] = (dk+1)*value / (value+dk*(1-db+db*bs.blog_length/avglength)) * math.log(blog_num/BlogFre[key])
-
-	while userinput!="EXIT":
-		userinput = raw_input("")
-		b = ta.getBlogByName(userinput)
-		Query = BlogVSM(userinput)
-
-		pid_list = b.getAllPosts()
-		
-		for pid in pid_list:
-			
-			p = ta.getPostById(user, pid)
-
-			terms = VA.extractTermsFromPost(p)
-
-			for term in terms:
-				Query.vector[term] += 1
-				Query.blog_length += 1
-
-		for key,value in Query.vector.iteritems():
-			Query.vector[key] = (dk+1)*value / (value+dk*(1-db+db*Query.blog_length/avglength))
-
-		for bs in Blogs:
-			temprel = 0
-			d_v_l = 0
-
+		for bs in self.Blogs:
+			temprel = float(0)
 			for key,value in bs.vector.iteritems():
 				temprel += value * Query.vector[key]
-				d_v_l += value
 
-			bs.relativity = temprel/d_v_l
+			bs.relativity = temprel/(bs.vector_d*Query.vector_d)
 
-		Blogs.sort(key=lambda x: x.relativity,reverse=True)
+		self.Blogs.sort(key=lambda x: x.relativity,reverse=True)
 
-		for i in range(10):
-			print "%d %s" %(i+1, Blogs[i].getBlogName())
+		return
+
 
 
 
