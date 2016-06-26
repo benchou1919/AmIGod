2d7c30f04ecb67d3b8773312ddd607c72edbecc1
Brian Huang <b01902068@ntu.edu.tw>
2016-06-26 15:39:38 +0800

    LM

--- LM.py
+++ LM.py
@@ -1,6 +1,7 @@
 import sys, os
 import pytumblr
 import json
+from math import log
 from tumblrUtil import TumblrAgent as TA
 from vocabUtil import VocabAgent as VA
 from imageUtil import parser, OCR
@@ -14,36 +15,98 @@ def legalImageType(url):
 
 if __name__ == "__main__":
 
-	output_file = open('EMresult', 'a')
+	output_file = open('LMresult', 'a')
+	input_file = open('../blogList', 'r').read().split('\n')
+
+	SMOOTHING = 0.2
+	Terms = []
+	wordCount = {}
 
 	# use TumblrAgent
 	ta = TA()
-	for bn in ta.getAllBlogs():
+	blogNames = ta.getAllBlogs()
+	for bn in blogNames:
+		wordCount[bn] = {}
+		wordCount[bn]['length'] = 0
+		wordCount[bn]['unique_length'] = 0
+		wordCount[bn]['words'] = {}
+		print "Parsing", bn
 		b = ta.getBlogByName(bn)
 		pid_list = b.getAllPosts()
-                cnt = 0
 		for pid in pid_list:
 			p = ta.getPostById(bn, pid)
-			if p.getType() == 'photo':
-				output_file.write('b=' + str(bn) + '\n')
-				output_file.write('p=' + str(pid) + '\n')
-				cnt += 1
-                                if cnt > 10:
-                                    break
-                                #ocrData = []
-				parserData = []
-				for photo in p.photos:
-					photoUrl = photo['original_size']['url']
-					if legalImageType(photoUrl):
-						#ocrData += OCR(photoUrl)
-						parserData += parser(photoUrl)
-				print bn, pid
-				#print str(ocrData)
-				print str(parserData)
-				print '\n'
-				#output_file.write('OCR=' + str(ocrData) + '\n')
-				output_file.write('caffe=' + str(parserData) + '\n')
-			# print p.blog_name, p.id, p.getType()
+			terms = VA.extractTermsFromPost(p)
+			# if p.getType() == 'photo':
+			# 	ocrData = []
+			# 	parserData = []
+			# 	for photo in p.photos:
+			# 		photoUrl = photo['original_size']['url']
+			# 		if legalImageType(photoUrl):
+			# 			ocrData += OCR(photoUrl)
+			# 			parserData += parser(photoUrl)
+			# 	terms += ocrData
+			# 	terms += parserData
+			# print bn, pid, terms
+			for term in terms:
+				if term not in wordCount[bn]['words']:
+					wordCount[bn]['words'][term] = 1
+				else:
+					wordCount[bn]['words'][term] += 1
+		wordCount[bn]['unique_length'] = len(wordCount[bn]['words'].keys())
+		for key in wordCount[bn]['words']:
+			if key not in Terms:
+				Terms.append(key)
+			wordCount[bn]['length'] += wordCount[bn]['words'][key]
+		print bn, wordCount[bn]['length'], wordCount[bn]['unique_length'], len(Terms)
 
+	# naive Bayes
+
+	# C_plus_D = float(len(Topic)+len(Label))
+	# def topicPriorProbability(topic):
+	# 	topicDocs = len(Train[topic])
+	# 	return float(1+topicDocs) / C_plus_D
+	
+	def wordProbability(word, blog):
+		wordInTopic = 0
+		if word in wordCount[blog]['words']:
+			wordInTopic = wordCount[blog]['words'][word]
+		blogLength = wordCount[blog]['length']
+		#print word, float(0.2+wordInTopic), "/", float(0.2*len(Terms)+blogLength), float(0.2+wordInTopic) / float(0.2*len(Terms)+blogLength)
+		return float(SMOOTHING+wordInTopic) / float(SMOOTHING*len(Terms)+blogLength)
+	
+	def countBlogProbability(blog, content):
+		# blog_P = log(topicPriorProbability(blog))
+		blog_P = 0
+		probability_pi = 0
+		for word in content:
+			probability_pi += log(wordProbability(word, blog))
+		return blog_P + probability_pi
+
+	def findMax(dictionary):
+		maxKey = ''
+		maxValue = -float("inf")
+		for key in dictionary:
+			print key, dictionary[key]
+			if dictionary[key] > maxValue:
+				maxValue = dictionary[key]
+				maxKey = key
+		return maxKey
+
+	# LM
+	for testBn in input_file:
+		b = ta.getBlogByName(testBn)
+		continue
+		pid_list = b.getAllPosts()
+		terms = []
+		for pid in pid_list:
+			p = ta.getPostById(testBn, pid)
+			terms += VA.extractTermsFromPost(p)
+		blogProbability = {}
+		for bn in blogNames:
+			blogProbability[bn] = countBlogProbability(bn, terms)
+		maxTopic = findMax(blogProbability)
+		for key, value in sorted(blogProbability.iteritems(), key=lambda (k,v): (v,k)):
+    		print "%s: %s" % (key, value)
+		
 	output_file.close()
 	sys.exit(0)
